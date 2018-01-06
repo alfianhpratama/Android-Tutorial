c01b32d0f425983317e695eb520d3c80424a9567
Agra8 <a_gra@outlook.com>
2018-01-06 20:07:18 +0700

    Update materi PdfView

--- app/src/main/java/alfianagramerry/mobpro/tugasbesar/androidtutorial/Lesson/Lesson1.java
+++ app/src/main/java/alfianagramerry/mobpro/tugasbesar/androidtutorial/Lesson/Lesson1.java
@@ -1,5 +1,6 @@
 package alfianagramerry.mobpro.tugasbesar.androidtutorial.Lesson;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.widget.PopupMenu;
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
 import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 import com.shockwave.pdfium.PdfDocument;
 
 import java.util.List;
@@ -33,12 +35,14 @@ public class Lesson1 extends Fragment implements View.OnClickListener, OnPageCha
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
     private static final String TAG = PdfView.class.getSimpleName();
     public static final String SAMPLE_FILE = "Sample01.pdf";
     final String title = "Sample01";
     PDFView pdfView;
     Integer pageNumber = 0;
     String pdfFileName;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -66,13 +70,13 @@ public class Lesson1 extends Fragment implements View.OnClickListener, OnPageCha
         });
         popupMenu.show();
     }
+
     private void displayFromAsset(String assetFileName) {
         pdfFileName = assetFileName;
 
         pdfView.fromAsset(SAMPLE_FILE)
                 .defaultPage(pageNumber)
                 .enableSwipe(true)
-
                 .swipeHorizontal(false)
                 .onPageChange(this)
                 .enableAnnotationRendering(true)
@@ -86,7 +90,6 @@ public class Lesson1 extends Fragment implements View.OnClickListener, OnPageCha
         pageNumber = page;
         getActivity().setTitle(String.format("%s %s / %s", title, page + 1, pageCount));
     }
-
 
     @Override
     public void loadComplete(int nbPages) {
