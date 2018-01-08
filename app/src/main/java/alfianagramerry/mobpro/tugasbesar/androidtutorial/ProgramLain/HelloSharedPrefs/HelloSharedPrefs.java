5b1d0366c90004fd6cd4dcd69010879d62327526
alfianhpratama <alfianhpratama@gmail.com>
2018-01-08 13:13:28 +0700

    Shared Pref

---
+++ app/src/main/java/alfianagramerry/mobpro/tugasbesar/androidtutorial/ProgramLain/HelloSharedPrefs/HelloSharedPrefs.java
@@ -0,0 +1,137 @@
+package alfianagramerry.mobpro.tugasbesar.androidtutorial.ProgramLain.HelloSharedPrefs;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import alfianagramerry.mobpro.tugasbesar.androidtutorial.R;
+
+public class HelloSharedPrefs extends AppCompatActivity {
+
+    private int mCount = 0;
+    // Current background color.
+    private int mColor;
+    // Text view to display both count and color.
+    private TextView mShowCountTextView;
+
+    // Key for current count
+    private final String COUNT_KEY = "count";
+    // Key for current color
+    private final String COLOR_KEY = "color";
+
+    // Shared preferences object
+    private SharedPreferences mPreferences;
+    // Name of shared preferences file
+    private static final String mSharedPrefFile = "com.example.android.hellosharedprefs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hellosharedprefs_activity_main);
+
+        setTitle("Hello Shared Prefs");
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Initialize views, color, preferences
+        mShowCountTextView = findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this, R.color.default_background);
+        mPreferences = getSharedPreferences(mSharedPrefFile, MODE_PRIVATE);
+
+        // Restore preferences
+        mCount = mPreferences.getInt(COUNT_KEY, 0);
+        mShowCountTextView.setText(String.format("%s", mCount));
+        mColor = mPreferences.getInt(COLOR_KEY, mColor);
+        mShowCountTextView.setBackgroundColor(mColor);
+    }
+
+    /**
+     * Callback for activity pause.  Shared preferences are saved in this callback.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt(COUNT_KEY, mCount);
+        preferencesEditor.putInt(COLOR_KEY, mColor);
+        preferencesEditor.apply();
+    }
+
+    /**
+     * Handles the onClick for the background color buttons.  Gets background color of the button
+     * that was clicked and sets the textview background to that color.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
+    }
+
+    /**
+     * Handles the onClick for the Count button.  Increments the value of the mCount global and
+     * updates the textview.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s", mCount));
+    }
+
+    /**
+     * Handles the onClick for the Reset button.  Resets the global count and background
+     * variables to the defaults, resets the views to those values, and clears the shared
+     * preferences
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void reset(View view) {
+        // Reset count
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        // Reset color
+        mColor = ContextCompat.getColor(this, R.color.default_background);
+        mShowCountTextView.setBackgroundColor(mColor);
+
+        // Clear preferences
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.clear();
+        preferencesEditor.apply();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.git_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.github:
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/merrymarst/KeyboardSamples/"));
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
+
